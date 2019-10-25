33103fbccfab39e29d305b13f3b222906dc632e6
Joel Harris <joel@zapatatechnology.com>
2019-10-25 12:04:16 -0400

    Report search

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,53 @@ import React, { Component } from 'react';
 import '../css/App.css';
 
 import AddReporting from './AddReporting';
+import ListReports from './ListReports';
 
+class App extends Component {
 
-function App() {
-  return (
-    <main className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <AddReporting />
-              {/* <div>Search Appointments</div>
+  constructor(){
+    super();
+    this.state = {
+      reports: []
+    };
+  }
+componentDidMount(){
+  fetch('./data.json')
+    .then(response => response.json())
+    .then(result => {
+      const rpts = result.map(item => {
+        return item;
+      });
+
+      this.setState({
+        reports: rpts
+      });
+    });
+
+  
+}
+  render() {
+
+   
+
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+           
+                <AddReporting />
+                <ListReports reports={this.state.reports}/>
+                {/* <div>Search Appointments</div>
               <div>List Appointments</div>  */}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
 
 export default App;
--- src/components/ListReports.js
+++ src/components/ListReports.js
@@ -2,7 +2,32 @@ import React, {Component} from 'react';
 
 class ListReports extends Component{
     render(){
-        return <div> List Reports </div>
+      
+        return (
+            <div className="appointment-list item-list mb-3">
+                {this.props.reports.map(report => (
+                    <div className="pet-item col media py-3">
+                    {/* <div className="mr-3">
+                      <button className="pet-delete btn btn-sm btn-danger">X</button>
+                    </div> */}
+        
+                    <div className="pet-info media-body">
+                      <div className="pet-head d-flex">
+                        <span className="pet-name">{report.serial.crc}</span>
+                        <span className="apt-date ml-auto">{report.acqDate}</span>
+                      </div>
+        
+                      <div className="owner-name">
+                        <span className="label-item">Subj: </span>
+                        <span>{report.subject} </span>
+                      </div>
+                      <div className="apt-notes">{report.text}</div>
+                    </div>
+                  </div>
+                ))}
+            
+        </div>
+        );
     }
 }
 
