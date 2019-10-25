9fe70cb7fda426a0f5453401b78fc2b69fee0543
Joel Harris <joel@zapatatechnology.com>
2019-10-25 09:14:26 -0400

    bootstrap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/umd/popper.js';
+import 'bootstrap/dist/js/bootstrap.js';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 
