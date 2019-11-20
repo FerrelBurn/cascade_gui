11fa8eddf642eb8fc2aead90b6ef1f379be5dfb1
Joel <joel@zapatatechnology.com>
2019-11-20 14:40:22 -0500

    added reqid

--- src/components/draftreport/ReqMatch.js
+++ src/components/draftreport/ReqMatch.js
@@ -10,7 +10,7 @@ class ReqMatch extends Component {
             <div className="">
                 <div className="row  " >
                     <div className="col">
-                   <b> Question: </b>{this.props.question}
+                   <b> Question: </b>{this.props.reqid} {this.props.question}
                     </div>
                     <br/>
                 </div>
