9b4adfe7be70b20d9949684e4c86b144c70e2650
Chris <cmontanaro9823@sm-nvdia-v100.zapatatechnology.com>
2020-07-14 12:42:58 -0400

    Fixed CSS rendering of AddRequirments

--- src/components/requirement/AddRequirement.js
+++ src/components/requirement/AddRequirement.js
@@ -1,95 +1,26 @@
 import React, {Component} from 'react';
 import Upload from '../Upload';
+import { Row, Col } from 'react-bootstrap';
 
 class AddRequirement extends Component{
     render(){
-        return (        <div className="card textcenter mt-3">
-        <div className="apt-addheading card-header bg-primary text-white">
-          Add Requirement
-        </div>
-
-        <div className="card-body">
-          {/* <form id="aptForm" noValidate>
-            <div className="form-group form-row">
-              <label
-                className="col-md-2 col-form-label text-md-right"
-                htmlFor="petName"
-                readOnly
-              >
-                Requirement Name
-              </label>
-              <div className="col-md-10">
-                <input
-                  type="text"
-                  className="form-control"
-                  name="petName"
-                  placeholder="Requirement Name"
-                />
-              </div>
-            </div>
-
-           
-            <div className="form-group form-row">
-              <label
-                className="col-md-2 col-form-label text-md-right"
-                htmlFor="aptDate"
-              >
-                Date
-              </label>
-              <div className="col-md-4">
-                <input
-                  type="date"
-                  className="form-control"
-                  name="aptDate"
-                  id="aptDate"
-                />
-              </div>
-              <label
-                className="col-md-2 col-form-label text-md-right"
-                htmlFor="aptTime"
-              >
-                Time
-              </label>
-              <div className="col-md-4">
-                <input
-                  type="time"
-                  className="form-control"
-                  name="aptTime"
-                  id="aptTime"
-                />
-              </div>
-            </div>
-
-            <div className="form-group form-row">
-              <label className="col-md-2 text-md-right" htmlFor="aptNotes">
-               Description
-              </label>
-              <div className="col-md-10">
-                <textarea
-                  className="form-control"
-                  rows="4"
-                  cols="50"
-                  name="aptNotes"
-                  id="aptNotes"
-                  placeholder="Description"
-                />
-              </div>
-            </div>
-
-            <div className="form-group form-row mb-0">
-              <div className="offset-md-2 col-md-10">
-                <button
-                  type="submit"
-                  className="btn btn-primary d-block ml-auto"
-                >
+        return (        
+	<>
+          <Row>
+            <Col md={{ span: 3, offset: 5 }}>
+              <div className="card textcenter mt-3">
+                <div className="apt-addheading card-header bg-primary text-white">
                   Add Requirement
-                </button>
+                </div>
+        
+                <div className="card-body">
+                  <Upload url="/nifi/requirements" />
+                </div>
               </div>
-            </div>
-          </form> */}
-          <Upload url="http://208.188.184.42:3000/requirements"/>
-        </div>
-      </div>)
+            </Col>
+          </Row>
+	</>
+	)
     }
 }
 
